84c2746966925ffc9ab9c0c01e6046e86fbb35ad
nguyenngocha1126 <nguyenngocha26112k4@gmail.com>
2026-04-13 21:40:04 +0700

    Update albums.js

--- src/views/albums/albums.js
+++ src/views/albums/albums.js
@@ -1,10 +1,32 @@
 import { SearchBar } from "../../components/searchBar.js";
+import { albums } from "../../data.js";
+import { AlbumCard } from "../../components/albumCard.js";
 
 export function AlbumsView() {
   const html = `
     <div class="container albums-view">
       ${SearchBar()}
+      ${AlbumsHeader()}
+      ${AlbumsGrid()}
+      ${AlbumsFooter()}
     </div>
   `;
   document.getElementById("view").innerHTML = html;
 }
+
+  function AlbumsHeader() {
+  return `
+    <div class="section-header row space-between">
+      <h6 class="bold max">Name ↓</h6>
+      <h6 class="bold">Artist</h6>
+      <h6 class="bold">Date</h6>
+    </div>
+  `;
+  }
+  function AlbumsGrid() {
+    return `
+      <div class="grid-list row wrap">
+        ${albums.map((album) => AlbumCard(album, "albums")).join("")}
+      </div>
+    `;
+  }
